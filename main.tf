72bedcf4aad753248b63576f13abe3eb7608b16a
pbeniwal <pbeniwal@yahoo.com>
2025-12-05 12:54:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
   name          = "tf-demo-pb-2929"
   location      = "us-central1"
-  project = "teak-facet-480011-s0 "
+  project = "teak-facet-480011-s0"
   force_destroy = true
   public_access_prevention = "enforced"
 }
